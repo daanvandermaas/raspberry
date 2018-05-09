6b3d0f7d49cf00f56c5552d4eed4a82b448ed46e
daanvandermaas <daanvandermaas@hotmail.com>
2018-05-09 15:41:05 +0200

    x

--- app.r
+++ app.r
@@ -1,2 +1,18 @@
 library(keras)
-print('check')
+library(jpeg)
+
+
+model = keras::load_model_hdf5('model')
+
+files = list.files(pattern = 'jpg')
+
+im = readJPEG(files[1])
+
+im = array(im, dim = c(1, dim(im)))
+
+im = aperm(im, c(1,3,2,4))
+
+model$predict_classes(im)
+
+
+
