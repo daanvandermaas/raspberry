08385981a20d6856c463460e293ccd98131d9c75
daanvandermaas <daanvandermaas@hotmail.com>
2018-05-15 13:46:03 +0200

    x

--- make_photo.r
+++ make_photo.r
@@ -1,1 +1,14 @@
-system('raspistill -o image.jpg -w 256 -h 256 --nopreview -t 10')
+
+i=0
+while(0<1){
+  print(i)
+  i = i+1
+#get time
+time = Sys.time()
+time = gsub(time , replacement =  '_', pattern = ' ')
+#take photo
+command = paste0('raspistill -o db/', i, '_', time ,'.jpg -w 512 -h 512 --nopreview -t 100')
+system(command)
+#get location
+
+}
