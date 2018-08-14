4f2539bffa278f669d7ab651a10c5fcd0be014a6
daanvandermaas <daanvandermaas@hotmail.com>
2018-08-14 11:08:02 +0200

    x

--- run_on_pi.r
+++ run_on_pi.r
@@ -65,7 +65,7 @@ file.remove(file_name_image)
 if(length(location)>2 & length(location_old)>2){
 #if sighting sent the following message
 if(pred == 1){
-q = paste0("INSERT INTO digitaalschouwen (time, prediction, location_x, location_y, location_old_x, location_old_y) VALUES (", time, ",", pred, ",", location[1], ",", location[2], "," , location_old[1], ",", location_old[2],")")
+q = paste0("INSERT INTO digitaalschouwen (time, prediction, location_x, location_y, location_old_x, location_old_y) VALUES ('", time, "',", pred, ",", location[1], ",", location[2], "," , location_old[1], ",", location_old[2],")")
 dbSendQuery(con , q)
 #sent photo as well
 }}
